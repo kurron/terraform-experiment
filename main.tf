c686db856c3eca4a17c5c2ce352988d98b6844cb
Ron Kurr <kurr@kurron.org>
2015-06-12 21:13:27 +0000

    Installed the load balancer

--- main.tf
+++ main.tf
@@ -107,3 +107,29 @@ resource "aws_instance" "docker" {
         command = "./provision-instance.sh ${aws_instance.docker.public_ip}"
     }
 }
+
+resource "aws_elb" "load-balancer" {
+  name = "load-balancer"
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:80/"
+    interval = 30
+  }
+
+  instances = ["${aws_instance.docker.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining_timeout = 400
+}
