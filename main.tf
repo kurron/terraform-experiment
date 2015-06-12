7e57c271d3d56652424e49634fcbe337e23e6e29
Ron Kurr <kurr@kurron.org>
2015-06-12 21:08:50 +0000

    Removed the outbound rules

--- main.tf
+++ main.tf
@@ -49,28 +49,8 @@ resource "aws_security_group_rule" "inbound-http" {
     security_group_id = "${aws_security_group.composable.id}"
 }
 
-resource "aws_security_group_rule" "outbound-http" {
-    type = "egress"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-    security_group_id = "${aws_security_group.composable.id}"
-}
-
 resource "aws_security_group_rule" "inbound-https" {
     type = "ingress"
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-    security_group_id = "${aws_security_group.composable.id}"
-}
-
-resource "aws_security_group_rule" "outbound-https" {
-    type = "egress"
     from_port = 443
     to_port = 443
     protocol = "tcp"
