cb9bb0928c2f5f18b97fdced0699fea031786223
Ron Kurr <kurr@kurron.org>
2015-06-13 16:17:10 +0000

    Improving ELB

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,41 @@
 
-output "id" {
+output "docker_id" {
   value = "${aws_instance.docker.id}"
 }
 
-output "az" {
+output "docker_az" {
   value = "${aws_instance.docker.availability_zone}"
 }
 
-output "placement" {
+output "docker_placement" {
   value = "${aws_instance.docker.placement_group}"
 }
 
-output "key" {
+output "docker_key" {
   value = "${aws_instance.docker.keyname}"
 }
 
-output "private_ip" {
+output "docker_private_ip" {
   value = "${aws_instance.docker.private_ip}"
 }
 
-output "public_dns" {
+output "docker_public_dns" {
   value = "${aws_instance.docker.public_dns}"
 }
 
-output "public_ip" {
+output "docker_public_ip" {
   value = "${aws_instance.docker.public_ip}"
 }
 
-output "security_groups" {
+output "docker_security_groups" {
   value = "${aws_instance.docker.security_groups}"
 }
 
-output "vpc_security_group_ids" {
+output "docker_vpc_security_group_ids" {
   value = "${aws_instance.docker.vpc_security_group_ids}"
 }
 
-output "subnet_id" {
+output "docker_subnet_id" {
   value = "${aws_instance.docker.subnet_id}"
 }
 
