8b05e5502b517b07fc9af0421868d5c6e6a88936
Ron Kurr <kurr@kurron.org>
2015-06-13 15:20:02 +0000

    Moving to using us-east as the default

--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "aws_region" {
+    description = "AWS region to launch servers."
+    default = "us-east-1"
+}
+
+variable "instance_type" {
+    description = "AWS EC2 instance type."
+    default = "t2.micro"
+}
+
 variable "key_name" {
     description = "Name of the SSH keypair to use in AWS."
     default = {
@@ -14,16 +24,6 @@ variable "key_path" {
         us-west-1 = "do-not-have-one-yet"
         us-west-2 = "/home/vagrant/aws/aws-linux.pem"
     }
-}
-
-variable "aws_region" {
-    description = "AWS region to launch servers."
-    default = "us-west-2"
-}
-
-variable "instance_type" {
-    description = "AWS EC2 instance type."
-    default = "t2.micro"
 }
 
 # Ubuntu Server 14.04 LTS (HVM), SSD Volume Type, 64-bit 
