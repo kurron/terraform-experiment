ab897977e521812ff56e714d6cd272f011b67911
Ron Kurr <kurr@kurron.org>
2015-06-13 16:52:14 +0000

    Added eu-west-1 support

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
     description = "AWS region to launch servers."
-    default = "us-west-2"
+    default = "eu-west-1"
 }
 
 variable "instance_type" {
@@ -14,6 +14,7 @@ variable "key_name" {
         us-east-1 = "us-east-1"
         us-west-1 = "us-west-1"
         us-west-2 = "us-west-2"
+        eu-west-1 = "eu-west-1"
     }
 }
 
@@ -23,6 +24,7 @@ variable "key_path" {
         us-east-1 = "/home/vagrant/aws/us-east-1.pem"
         us-west-1 = "/home/vagrant/aws/us-west-1.pem"
         us-west-2 = "/home/vagrant/aws/us-west-2.pem"
+        eu-west-1 = "/home/vagrant/aws/eu-west-1.pem"
     }
 }
 
@@ -33,6 +35,7 @@ variable "aws_amis" {
         us-east-1 = "ami-d05e75b8"
         us-west-1 = "ami-df6a8b9b"
         us-west-2 = "ami-5189a661"
+        eu-west-1 = "ami-47a23a30"
     }
 }
 
