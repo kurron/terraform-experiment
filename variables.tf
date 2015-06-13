516886decca8a72d5ca7fccfba45cd1f3dafef63
Ron Kurr <kurr@kurron.org>
2015-06-13 17:27:59 +0000

    Added ap-northeast-1 support

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
     description = "AWS region to launch servers."
-    default = "sa-east-1"
+    default = "ap-northeast-1"
 }
 
 variable "instance_type" {
@@ -11,24 +11,28 @@ variable "instance_type" {
 variable "key_name" {
     description = "Name of the SSH keypair to use in AWS."
     default = {
-        us-east-1    = "us-east-1"
-        us-west-1    = "us-west-1"
-        us-west-2    = "us-west-2"
-        eu-west-1    = "eu-west-1"
-        eu-central-1 = "eu-central-1"
-        sa-east-1    = "sa-east-1"
+        us-east-1      = "us-east-1"
+        us-west-1      = "us-west-1"
+        us-west-2      = "us-west-2"
+        eu-west-1      = "eu-west-1"
+        eu-central-1   = "eu-central-1"
+        sa-east-1      = "sa-east-1"
+        ap-southeast-1 = "ap-southeast-1"
+        ap-northeast-1 = "ap-northeast-1"
     }
 }
 
 variable "key_path" {
     description = "Path to the private portion of the SSH key specified."
     default = {
-        us-east-1    = "/home/vagrant/aws/us-east-1.pem"
-        us-west-1    = "/home/vagrant/aws/us-west-1.pem"
-        us-west-2    = "/home/vagrant/aws/us-west-2.pem"
-        eu-west-1    = "/home/vagrant/aws/eu-west-1.pem"
-        eu-central-1 = "/home/vagrant/aws/eu-central-1.pem"
-        sa-east-1    = "/home/vagrant/aws/sa-east-1.pem"
+        us-east-1      = "/home/vagrant/aws/us-east-1.pem"
+        us-west-1      = "/home/vagrant/aws/us-west-1.pem"
+        us-west-2      = "/home/vagrant/aws/us-west-2.pem"
+        eu-west-1      = "/home/vagrant/aws/eu-west-1.pem"
+        eu-central-1   = "/home/vagrant/aws/eu-central-1.pem"
+        sa-east-1      = "/home/vagrant/aws/sa-east-1.pem"
+        ap-southeast-1 = "/home/vagrant/aws/ap-southeast-1.pem"
+        ap-northeast-1 = "/home/vagrant/aws/ap-northeast-1.pem"
     }
 }
 
@@ -36,12 +40,14 @@ variable "key_path" {
 variable "aws_amis" {
     description = "AMI to build the instance from."
     default = {
-        us-east-1    = "ami-d05e75b8"
-        us-west-1    = "ami-df6a8b9b"
-        us-west-2    = "ami-5189a661"
-        eu-west-1    = "ami-47a23a30"
-        eu-central-1 = "ami-accff2b1"
-        sa-east-1    = "ami-4d883350"
+        us-east-1      = "ami-d05e75b8"
+        us-west-1      = "ami-df6a8b9b"
+        us-west-2      = "ami-5189a661"
+        eu-west-1      = "ami-47a23a30"
+        eu-central-1   = "ami-accff2b1"
+        sa-east-1      = "ami-4d883350"
+        ap-southeast-1 = "ami-96f1c1c4"
+        ap-northeast-1 = "ami-936d9d93"
     }
 }
 
